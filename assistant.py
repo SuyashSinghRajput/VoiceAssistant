140ec2fd52b8f8a00cd9e11ce4ffb17f29c71b40
SuyashSinghRajput <rajputsuyashsingh5@gmail.com>
2025-08-28 00:10:36 +0530

    feat: add app launch support with wake word and ESC/stop exit

--- assistant.py
+++ assistant.py
@@ -1,16 +1,39 @@
 from core.speech import listen, speak
 from core.commands import handle_command
+import keyboard
 
 def main():
-    speak("Hello sir i am JARVIS your Assistant. Say something!")
+    speak("Say 'Jarvis' to wake me up, or press ESC to exit.")
+    
     while True:
+        if keyboard.is_pressed("esc"):
+            speak("Goodbye Sir")
+            print("Exiting Code")
+            break
+
         cmd = listen()
         if not cmd:
             continue
-        if "stop" in cmd:
-            speak("Goodbye!")
-            break
-        handle_command(cmd)
+        cmd = cmd.lower()
+
+        if "jarvis" in cmd:
+            speak("Hello sir, I am JARVIS your assistant. What should I do?")
+            
+            while True:
+                sub_cmd = listen()
+                if not sub_cmd:
+                    continue
+                sub_cmd = sub_cmd.lower()
+
+                if "stop" in sub_cmd:
+                    speak("Goodbye!")
+                    return
+
+                handle_command(sub_cmd)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        speak("Goodbye Sir")
+        print("Program stopped by user")
--- core/commands.py
+++ core/commands.py
@@ -1,5 +1,13 @@
 import webbrowser
 from core.speech import speak
+import subprocess
+import os
+
+
+apps = {
+    "Github": r"C:\Users\rajpu\AppData\Local\GitHubDesktop\GitHubDesktop.exe",
+    "Notepad": "notepad.exe"
+}
 
 def handle_command(command):
     if "open browser" in command:
@@ -8,5 +16,15 @@ def handle_command(command):
     elif "youtube" in command:
         speak("Opening YouTube")
         webbrowser.open("https://www.youtube.com")
+    elif "open" in command:
+        command = command.lower()
+        for app in apps:
+            if app.lower() in command:
+                subprocess.Popen(apps[app])
+                speak("Opening Github Sir")
+                return
+            speak("Can't Find the App")
+            
+
     else:
         speak("Sorry, I don't know how to do that yet.")
